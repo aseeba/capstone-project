1ad579ccff73566af062ae5881aebfe191b794fa
Anja Seeba <ahoi@achtsam.media>
2021-03-09 12:40:29 +0100

    Remove redundant files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,3 @@
-import GlobalStyle from '../src/GlobalStyle'
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  viewport: {
-    viewports: INITIAL_VIEWPORTS,
-    defaultViewport: 'iphone6',
-  },
 }
-
-export const decorators =[
-  Story => (
-    <>
-      <GlobalStyle />
-      <Story />
-    </>
-  ),
-]
